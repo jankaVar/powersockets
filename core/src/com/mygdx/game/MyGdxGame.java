f566a44ee97f529bde1ce186b91609fad9a1d190
moru <e1028092@student.tuwien.ac.at>
2016-05-07 17:53:18 +0200

    Rendering generators and sockets.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,16 +6,16 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Gdx2DPixmap;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 
 public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
     SpriteBatch batch;
-    Texture img;
+    Texture socketImg;
+    Texture generatorImg;
     Level level;
-    float red = 1.0f;
+    float red = 0.0f;
 
     public MyGdxGame() {
 
@@ -25,7 +25,8 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
     @Override
     public void create () {
         this.batch = new SpriteBatch();
-        this.img = new Texture("badlogic.jpg");
+        this.socketImg = new Texture("socket.png");
+        this.generatorImg = new Texture("generator.png");
 
         Gdx.input.setInputProcessor(this);
 
@@ -46,12 +47,6 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
 
     @Override
     public void render () {
-        float blue = 0.0f;
-        if(Gdx.input.isKeyPressed(Input.Keys.A))
-            blue = 1.0f;
-
-
-
         final int screenHeight = Gdx.graphics.getHeight();
         final int screenWidth = Gdx.graphics.getWidth();
         final int drawAreaSize = Math.min(screenHeight, screenWidth);
@@ -64,7 +59,7 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
         matrix.setToOrtho2D(0, 0, screenWidth, screenHeight);
         batch.setProjectionMatrix(matrix);
 
-        Gdx.gl.glClearColor(this.red, blue, 0.5f, 1);
+        Gdx.gl.glClearColor(this.red, 0.02734375f, 0.16796875f, 1.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clears the screen
 
         batch.begin();
@@ -72,7 +67,17 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
         for(Socket socket : this.level.getSockets()) {
             Vector2 center = socket.getCenter();
             batch.draw(
-                img,
+                socketImg,
+                center.x * drawAreaSize - SOCKET_SIZE / 2,
+                center.y * drawAreaSize - SOCKET_SIZE / 2,
+                SOCKET_SIZE,
+                SOCKET_SIZE
+            );
+        }
+        for(Generator generator : this.level.getGenerators()) {
+            Vector2 center = generator.getCenter();
+            batch.draw(
+                generatorImg,
                 center.x * drawAreaSize - SOCKET_SIZE / 2,
                 center.y * drawAreaSize - SOCKET_SIZE / 2,
                 SOCKET_SIZE,
@@ -80,9 +85,9 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
             );
         }
 
-        //batch.draw(img, 0.0f, 0.0f);
-        //batch.draw(img, 0.0f, 0.0f, 100.0f, 100.0f);
-        //batch.draw(img, 0.0f, 0.0f, SOCKET_SIZE, SOCKET_SIZE);
+        //batch.draw(socketImg, 0.0f, 0.0f);
+        //batch.draw(socketImg, 0.0f, 0.0f, 100.0f, 100.0f);
+        //batch.draw(socketImg, 0.0f, 0.0f, SOCKET_SIZE, SOCKET_SIZE);
         batch.end();
     }
     /// </application-adapter>
